84a6fd5d6d2ad6cd1af389dee7caa65a8d5c56be
Justin Green <justingreendev@gmail.com>
2023-02-22 23:23:53 -0600

    Update app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,7 +2,9 @@ import '@/styles/app.scss';
 import { GlobalProvider } from '@/context/GlobalContext';
 
 export default function App({ Component, pageProps }) {
-  return <GlobalProvider>
-    <Component {...pageProps} />
-  </GlobalProvider>
+  return (
+    <GlobalProvider>
+      <Component {...pageProps} />
+    </GlobalProvider>
+  )
 }
