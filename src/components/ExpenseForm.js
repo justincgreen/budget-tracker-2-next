08985260c36052e6b242c2255480c4c096c7a8cb
Justin Green <justingreendev@gmail.com>
2023-03-03 10:28:12 -0600

    Capture expense form data

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,11 +1,30 @@
-import { useContext } from 'react';
+import { useState, useContext } from 'react';
 import GlobalContext from '@/context/GlobalContext';
 
 const ExpenseForm = () => {
 	const { displayExpenseForm, setDisplayExpenseForm } = useContext(GlobalContext);
 	
+	const [expenseDescription, setExpenseDescription] = useState('');
+	const [expenseAmount, setExpenseAmount] = useState(0);
+	
 	const handleDisplayForm = () => {
 		setDisplayExpenseForm(!displayExpenseForm);
+	}
+	
+	const captureExpenseDescription = (e) => {
+		setExpenseDescription(e.target.value);
+	}
+	
+	const captureExpenseAmount = (e) => {
+		setExpenseAmount(e.target.value);
+	}
+	
+	const submitForm = (e) => {
+		const formInput = document.querySelectorAll('.form__input');
+		
+		e.preventDefault();		
+		formInput.forEach(input => input.value = ''); // Clear input fields on submit
+		//console.log(expenseDescription, expenseAmount);
 	}
 	
 	const listUpper = () => {
@@ -19,12 +38,12 @@ const ExpenseForm = () => {
 	
 	const renderForm = () => {
 		return (
-			<form>
+			<form className="form__add-expense">
 				<label className="form__label">Description</label>
-				<input type="text" placeholder="Enter Description" className="form__input form__input--100" />
+				<input type="text" placeholder="Enter Description" className="form__input form__input--100" onChange={captureExpenseDescription} />
 				<label className="form__label">Amount</label>
-				<input type="text" placeholder="Enter Amount" className="form__input form__input--100" />
-				<button className="button">Add Expense</button>
+				<input type="number" min="0" placeholder="Enter Amount" className="form__input form__input--100" onChange={captureExpenseAmount} />
+				<button className="button" onClick={submitForm}>Add Expense</button>
 			</form>
 		)
 	}
