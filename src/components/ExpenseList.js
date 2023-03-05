049ea75d563d63514a14ffa643810858c2a9d096
Justin Green <justingreendev@gmail.com>
2023-03-04 19:10:41 -0600

    Added expense item styling

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,6 +1,8 @@
 import { useState, useContext } from 'react';
 import GlobalContext from '@/context/GlobalContext';
 import ExpenseForm from '@/components/ExpenseForm';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import EditNotificationsIcon from '@mui/icons-material/EditNotifications';
 
 const ExpenseList = () => {
 	const { displayExpenseForm, setDisplayExpenseForm } = useContext(GlobalContext);
@@ -9,7 +11,15 @@ const ExpenseList = () => {
 		setDisplayExpenseForm(!displayExpenseForm);
 	}
 	
-	const listUpper = () => {
+	const deleteExpense = () => {
+		console.log('Deleted');
+	}
+	
+	const editExpense = () => {
+		console.log('Edit Expense');
+	}
+	
+	const renderListUpper = () => {
 		return (
 			<div className="c-expense-list__upper">
 				<h2 className="c-expense-list__title">Expense List</h2> 
@@ -18,11 +28,31 @@ const ExpenseList = () => {
 		)
 	}
 	
+	const renderList = () => {
+		return (
+			<ul className="c-expense-list__items">
+				<li className="c-expense-list__item">
+					<span className="c-expense-list__item-timestamp">3/4/23</span>
+					<span className="c-expense-list__description">Food</span>
+					<span className="c-expense-list__delete" onClick={deleteExpense}>
+						<DeleteForeverIcon sx={{ color: '#ff4e4e' }} />
+					</span>
+					<span className="c-expense-list__edit" onClick={editExpense}>
+						<EditNotificationsIcon sx={{ color: '#55d4da' }} />
+					</span>					
+				</li>
+			</ul>
+		)
+	}
+		
 	return (
 		<div className="c-expense-list">			
 			{
-				displayExpenseForm ? <ExpenseForm /> : listUpper()
-			}		
+				displayExpenseForm ? <ExpenseForm /> : renderListUpper()
+			}
+			{
+				!displayExpenseForm && renderList()
+			}
 		</div>
 	)
 }
