c8930c1645f5b5b110d567bb9cff98ac2fd7dd84
Justin Green <justingreendev@gmail.com>
2023-03-24 11:01:42 -0500

    Fixing spacing

--- src/pages/expenses.js
+++ src/pages/expenses.js
@@ -9,28 +9,28 @@ import ExpenseList from '@/components/ExpenseList';
 
 export default function Expenses() {	
   return (
-	  <>
-		  <Head>
-			  <title>Expenses</title>
-			  <meta name="description" content="Expense List" />
-			  <meta name="viewport" content="width=device-width, initial-scale=1" />
-			  <link rel="icon" href="/favicon.ico" />
-		  </Head>
-		  
-		  <main className="main">
-			  <Navigation />				
-			  
-			  <div className="page__wrapper">
-				  <Navicon />
-				  <ExpensesBlock />
-				  
-				  <section className="section__container">
-					  <section className="section__content">
-						  <ExpenseList />
-					  </section>
-				  </section>
-			  </div>
-		  </main>
-	  </>
+  <>
+    <Head>
+    <title>Expenses</title>
+    <meta name="description" content="Expense List" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="icon" href="/favicon.ico" />
+    </Head>
+    
+    <main className="main">
+      <Navigation />				
+      
+        <div className="page__wrapper">
+          <Navicon />
+          <ExpensesBlock />
+        
+        <section className="section__container">
+          <section className="section__content">
+          <ExpenseList />
+          </section>
+        </section>
+      </div>
+    </main>
+  </>
   )
 }
