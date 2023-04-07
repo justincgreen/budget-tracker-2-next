21c2212d9dff325ca2e7b5f133bf176853b72392
Justin Green <justingreendev@gmail.com>
2023-04-07 10:28:53 -0500

    Added data for chart

--- src/pages/chart.js
+++ src/pages/chart.js
@@ -13,27 +13,24 @@ import Navicon from '@/components/Navicon';
 import Footer from '@/components/Footer';
 
 export default function Chart() {
-  const donutData = {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+  const { 
+     globalIncome,
+     globalExpenses
+   } = useContext(GlobalContext);
+   
+  const data = {
+    labels: ['Income', 'Expenses'],
     datasets: [
       {
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)',
+        data: [globalIncome, globalExpenses],
+        label: '$',
+        backgroundColor: [          
+          'rgba(0, 255, 6, 0.4)',
+          'rgba(255, 76, 71, 0.4)'        
         ],
         borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)',
+          '#00ff06',
+          '#ff4c47'
         ],
         borderWidth: 1,
       },
@@ -57,8 +54,9 @@ export default function Chart() {
           
           <section className="section__container">
             <section className="section__content">
-              <h1>Chart Page</h1>
-              <Doughnut data={donutData} />
+              <div className="chart-wrapper">
+                <Doughnut data={data} />
+              </div>
             </section>
           </section>
           
